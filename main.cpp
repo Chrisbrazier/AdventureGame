42337018242d8aebc0e2d65943856edf93bafc3a
ChrisBrazier <cbrazier02@qub.ac.uk>
2021-12-29 22:09:48 +0000

    Added player menu loop

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
 
     // Explain the game to the user, and ask them to pick a hero name 
 
-    std::cout << "\nDungeon crawler is a text based adventure game, where you'll choose your own path. \nAre you ready to explore the dungeons??";
+    std::cout << "\nDungeon crawler is a text based adventure game, where you'll choose your own path. \nAre you ready to explore the dungeons??\n";
 
     std::cout << "\nFirst, lets come up with a name for your hero: \n";
     
@@ -19,22 +19,28 @@ int main() {
     std::cin >> name;
 
     // Confirm the name selection
-    std::cout << "Your hero will be called " << name << "." << "\n Its time to let the adventure begin!\n";
+    std::cout << "Your hero will be called " << name << "." << "\nIts time to let the adventure begin!\n";
     std::cout << "You approach the entrance to the Dungeon. A sign about the threshold holds a warning to anyone who would enter\n";
     std::cout << "Be wary all ye who cross this doorway. A curse lies here.\n";
     
     int progress;
    
+   
+    std::cout << "Press 1 to start a new game, or press 2 to exit\n";
+    std::cin >> progress;
+   while(progress != 1 & progress != 2) {
+       std::cout << "Please pick a valid option!\n";
+       std::cin >> progress;
+   }
 
-    std::cout << "Press 1 to start a new game, or press 2 to exit";
+   if(progress == 1){
+       std::cout << "Let the adventure begin!\n";
+   } else if(progress == 2) {
+       std::cout << "Have it your way, scaredy cat!\n";
+       return 0;
 
-    if(progress == 1) {
-        std::cout << "Let the adventure begin!"; 
-    } else if(progress == 2) {
-        std::cout << "You're a scaredy bastard.";
-    }
-
-
+   }
+   
 
     // Store player choices 
     int Choice;
@@ -45,7 +51,7 @@ int main() {
     std::cout << "Ok " << name << " its up to you now. What would you like to do?\n\n";
     std::cout << "1). Run headfirst into the dungeon, throwing aside all caution..\n";
     std::cout << "2). Slowly inch your way into the entrance of the cavernous Dungeon..\n";
-    std::cout << "3). Have a panic attack, turn around and exit. No gold is worth this..";
+    std::cout << "3). Have a panic attack, turn around and exit. No gold is worth this..\n";
 
     if(Choice == 1) {
 

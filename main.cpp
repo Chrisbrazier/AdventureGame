f4cff4964ee51ed2ba72588c7550e054b926c699
ChrisBrazier <cbrazier02@qub.ac.uk>
2021-12-29 21:26:45 +0000

    Started user menu options

--- main.cpp
+++ main.cpp
@@ -16,16 +16,49 @@ int main() {
     
     // Store the users name 
     char name [20];
-    char enter [5];
     std::cin >> name;
 
     // Confirm the name selection
     std::cout << "Your hero will be called " << name << "." << "\n Its time to let the adventure begin!\n";
-
     std::cout << "You approach the entrance to the Dungeon. A sign about the threshold holds a warning to anyone who would enter\n";
     std::cout << "Be wary all ye who cross this doorway. A curse lies here.\n";
-    std::cout << "Press enter to continue into the Dungeon\n";
     
+    int progress;
+   
+
+    std::cout << "Press 1 to start a new game, or press 2 to exit";
+
+    if(progress == 1) {
+        std::cout << "Let the adventure begin!"; 
+    } else if(progress == 2) {
+        std::cout << "You're a scaredy bastard.";
+    }
+
+
+
+    // Store player choices 
+    int Choice;
+    
+
+
+    // Present player with first options 
+    std::cout << "Ok " << name << " its up to you now. What would you like to do?\n\n";
+    std::cout << "1). Run headfirst into the dungeon, throwing aside all caution..\n";
+    std::cout << "2). Slowly inch your way into the entrance of the cavernous Dungeon..\n";
+    std::cout << "3). Have a panic attack, turn around and exit. No gold is worth this..";
+
+    if(Choice == 1) {
+
+    } else if(Choice == 2) {
+
+    } else if(Choice == 3) {
+
+    } else if(Choice != 1 || 2 || 3) {
+        std::cout << "Pick a real option you dummy."; 
+    }
+
+
+
 
 
 
